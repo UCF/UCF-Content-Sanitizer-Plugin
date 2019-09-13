45abc5b00bdb5d46a11f4b868b6cac3665c3b821
Jo Dickson <jogreybill@gmail.com>
2019-09-13 16:57:36 -0400

    Initial commit

---
+++ ucf-content-sanitizer-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Content Sanitizer Plugin
+Description: Provides filters and utilities for sanitizing WordPress post content.
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/UCF-Content-Sanitizer-Plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
